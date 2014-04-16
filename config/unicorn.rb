b96bf79b286a1d94a69e0a66af50188464722632
Sean Handley <sean.handley@gmail.com>
2014-04-16 13:15:39 +0100

    Unicorn config.

---
+++ config/unicorn.rb
@@ -0,0 +1,19 @@
+# Set the working application directory
+working_directory "/home/rails/soleman"
+
+# Unicorn PID file location
+pid "/var/run/rails/unicorn.pid"
+
+# Path to logs
+stderr_path "/var/log/rails/unicorn.err.log"
+stdout_path "/var/log/rails/unicorn.log"
+
+# Unicorn socket
+listen "/var/run/rails/unicorn.sock"
+
+# Number of processes
+# worker_processes 4
+worker_processes 2
+
+# Time-out
+timeout 30
