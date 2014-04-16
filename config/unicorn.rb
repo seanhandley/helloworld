7c508bec8ed79e803d129aae3ab22256fe5bbc03
Sean Handley <sean.handley@gmail.com>
2014-04-16 19:26:55 +0100

    Keep it simple, stupid.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -9,7 +9,7 @@ stderr_path "/var/log/rails/unicorn.err.log"
 stdout_path "/var/log/rails/unicorn.log"
 
 # Unicorn socket
-listen "/var/run/rails/unicorn.sock"
+#listen "/var/run/rails/unicorn.sock"
 listen "127.0.0.1:8080"
 
 # Number of processes
